0f4a3b569e6d61db402cef1e45814f2f2c9fc75b
Carlos Escalante <cescalante2988@gmail.com>
2018-10-20 09:16:30 -0600

    agregando clases 01 y 02

---
+++ clase01/ejemplo_01.py
@@ -0,0 +1,11 @@
+# Esto es un ejemplo
+# esta linea hace esto, etc...
+'''
+@author Carlos Escalante
+esto es un bloque de comentario
+multilinea...
+'''
+
+
+print("Hola Mundo")
+print("Como estas", "muy bien")
---
+++ clase02/readme.md
@@ -0,0 +1,3 @@
+pip install tox
+
+pip install pytest
